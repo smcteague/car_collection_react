74e902f31d1b08c95588e2553cf6b94f7d7fae92
Steve McTeague <smcteague@yahoo.com>
2022-11-29 08:45:52 -0600

    updated commit

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -15,6 +15,12 @@ const Header = styled('h1')({
     alignItems: 'center'
 })
 
+const Header3 = styled('h3')({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+})
+
 const Image = styled('img')({
     height: 'auto',
     width: '100%'
@@ -26,6 +32,7 @@ export const Dashboard = () => {
     return (
         <Root>
             <Header>Dashboard</Header>
+            <Header3>Check out my neat-o car collection!</Header3>
 
             <Box sx={{ flexGrow: 1 }} >
                 <Grid container spacing={4} >
