82bfa5b50dadc5ec80df548c3f021e1cea6331e5
Steve McTeague <smcteague@yahoo.com>
2022-11-28 21:56:30 -0600

    updated commit

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { styled } from '@mui/system';
+import { Box, Grid, } from '@mui/material';
+
+const Root = styled('div')({
+    padding: 0,
+    margin: 0
+})
+
+
+
+
+
 
 export const Dashboard = () => {
     return (
-        <div>
+        <Root>
             <h1>Dashboard</h1>
-        </div>
+
+            <Box sx={{ flexGrow: 1 }} >
+                {/* Setting up the Fluid Grid system */}
+                <Grid container spacing={4} >
+                    <Grid item xs={6}>
+                        <div className='grid-elements'>Car 1</div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className='grid-elements'>Car 2</div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className='grid-elements'>Car 3</div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className='grid-elements'>Car 4</div>
+                    </Grid>
+                </Grid>
+            </Box>
+
+        </Root>
     )
 }
 
