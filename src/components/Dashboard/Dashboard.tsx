10f19753a8decf9e83a602c9e85ea870ed0a279d
Steve McTeague <smcteague@yahoo.com>
2022-11-29 08:48:18 -0600

    updated commit

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,6 +4,11 @@ import { Box, Grid, Card } from '@mui/material';
 
 import car_image_placeholder from '../../assets/images/undraw_by_my_car_re_j3jt.svg'
 
+const Page = styled('body')({
+    margin: '12px',
+    padding: '0'
+})
+
 const Root = styled('div')({
     padding: 0,
     margin: 0
@@ -30,95 +35,97 @@ const Image = styled('img')({
 
 export const Dashboard = () => {
     return (
-        <Root>
-            <Header>Dashboard</Header>
-            <Header3>Check out my neat-o car collection!</Header3>
+        <Page>
+            <Root>
+                <Header>Dashboard</Header>
+                <Header3>Check out my neat-o car collection!</Header3>
 
-            <Box sx={{ flexGrow: 1 }} >
-                <Grid container spacing={4} >
-                    <Grid item xs={6}>
-                        <Card variant="outlined">Car 1
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
+                <Box sx={{ flexGrow: 1 }} >
+                    <Grid container spacing={4} >
+                        <Grid item xs={6}>
+                            <Card variant="outlined">Car 1
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 2
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 3
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 4
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 5
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 6
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 7
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card variant="outlined">Car 8
+                                <Image
+                                    src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={'item.title'}
+                                    loading="lazy"
+                                />                        
+                            </Card>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 2
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 3
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 4
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 5
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 6
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 7
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                    <Card variant="outlined">Car 8
-                            <Image
-                                src={`${car_image_placeholder}?w=248&fit=crop&auto=format`}
-                                srcSet={`${car_image_placeholder}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                alt={'item.title'}
-                                loading="lazy"
-                            />                        
-                        </Card>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Root>
+                </Box>
+            </Root>
+        </Page>
     )
 }
 
