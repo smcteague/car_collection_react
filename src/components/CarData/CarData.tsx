3e77e795b376e0c8b5c7efc04dcba465f5f6a382
Steve McTeague <smcteague@yahoo.com>
2022-12-01 19:09:04 -0600

    updated commit

--- src/components/CarData/CarData.tsx
+++ src/components/CarData/CarData.tsx
@@ -54,100 +54,110 @@ export const Album = () => {
         getData();
     }
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <main>
-                <Box 
-                    sx = {{
-                        bgcolor: 'background.paper',
-                        pt: 8,
-                        pb: 6,
-                    }}
-                >
-                    <Container maxWidth="sm">
-                        <Typography 
-                            component = 'h1'
-                            variant = 'h2'
-                            align = 'center'
-                            color = 'text.primary'
-                            gutterBottom                     
-                        >
-                            My Car Collection
-                        </Typography>
-                        <Typography variant='h5' align='center' color='text.secondary' paragraph>
-                            This is my neat-o car collection!
-                        </Typography>
-                        <Stack
-                            sx = {{ pt: 4 }}
-                            direction = {{sm:'column', md:'row'}}
-                            spacing = {2}
-                            justifyContent = 'center'
-                        >
+    const MyAuth = localStorage.getItem('myAuth');
+    console.log(MyAuth);
+    if (MyAuth == 'true') {
+        return (
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <main>
+                    <Box 
+                        sx = {{
+                            bgcolor: 'background.paper',
+                            pt: 8,
+                            pb: 6,
+                        }}
+                    >
+                        <Container maxWidth="sm">
+                            <Typography 
+                                component = 'h1'
+                                variant = 'h2'
+                                align = 'center'
+                                color = 'text.primary'
+                                gutterBottom                     
+                            >
+                                My Car Collection
+                            </Typography>
+                            <Typography variant='h5' align='center' color='text.secondary' paragraph>
+                                This is my neat-o car collection!
+                            </Typography>
+                            <Stack
+                                sx = {{ pt: 4 }}
+                                direction = {{sm:'column', md:'row'}}
+                                spacing = {2}
+                                justifyContent = 'center'
+                            >
 
-                        <Button variant='contained' onClick={handleDialogClickOpen}>Add New Car</Button>
-                        <Dialog open={dialogOpen} onClose={handleDialogClickClose} aria-labelledby='form-dialog-title'>
-                        <DialogTitle id='form-dialog-title'>Add New Car</DialogTitle>
-                        <DialogContent>
-                            <DialogContentText>Add a New Car</DialogContentText>
-                            <CarForm />
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleDialogClickClose} color='primary'>Cancel</Button>
-                        </DialogActions>
-                        </Dialog>
+                            <Button variant='contained' onClick={handleDialogClickOpen}>Add New Car</Button>
+                            <Dialog open={dialogOpen} onClose={handleDialogClickClose} aria-labelledby='form-dialog-title'>
+                            <DialogTitle id='form-dialog-title'>Add New Car</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>Add a New Car</DialogContentText>
+                                <CarForm />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleDialogClickClose} color='primary'>Cancel</Button>
+                            </DialogActions>
+                            </Dialog>
 
-                        </Stack>
+                            </Stack>
+                        </Container>
+                    </Box>
+                    <Container sx={{ py: 8 }} maxWidth='md'>
+                        <Grid container spacing={4}>
+                            {carData.map((car: { id: string, make: string, model: string, year: string, color: string, mileage: number, price: number  }) => (
+                                <Grid item key={car.id} xs={12} sm={6} md={4}>
+                                    <Card 
+                                        sx = {{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography gutterBottom variant='h5' component='h2'>
+                                                {car.year}
+                                            </Typography>
+                                            <Typography>
+                                                {car.make}
+                                            </Typography>
+                                            <Typography>
+                                                {car.model}
+                                            </Typography>
+                                            <Typography>
+                                                Color: {car.color}
+                                            </Typography>
+                                            <Typography>
+                                                Mileage: {car.mileage}
+                                            </Typography>
+                                            <Typography>
+                                                Price: {car.price}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button onClick={handleOpen} size='small'>Update</Button>
+                                            <Button onClick={() => deleteData(car.id)} size='small'>Delete</Button>
+                                        </CardActions>
+                                        <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
+                                            <DialogTitle id='form-dialog-title'>Update a Car</DialogTitle>
+                                            <DialogContent>
+                                                <DialogContentText>Car id: {car.id}</DialogContentText>
+                                                <CarForm id={car.id} />
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <Button onClick={handleClose} color='primary'>Cancel</Button>
+                                            </DialogActions>
+                                        </Dialog>
+                                    </Card>
+                                </Grid>
+                        ))}
+                        </Grid>
                     </Container>
-                </Box>
-                <Container sx={{ py: 8 }} maxWidth='md'>
-                    <Grid container spacing={4}>
-                        {carData.map((car: { id: string, make: string, model: string, year: string, color: string, mileage: number, price: number  }) => (
-                            <Grid item key={car.id} xs={12} sm={6} md={4}>
-                                <Card 
-                                    sx = {{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                >
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant='h5' component='h2'>
-                                            {car.year}
-                                        </Typography>
-                                        <Typography>
-                                            {car.make}
-                                        </Typography>
-                                        <Typography>
-                                            {car.model}
-                                        </Typography>
-                                        <Typography>
-                                            Color: {car.color}
-                                        </Typography>
-                                        <Typography>
-                                            Mileage: {car.mileage}
-                                        </Typography>
-                                        <Typography>
-                                            Price: {car.price}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button onClick={handleOpen} size='small'>Update</Button>
-                                        <Button onClick={() => deleteData(car.id)} size='small'>Delete</Button>
-                                    </CardActions>
-                                    <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
-                                        <DialogTitle id='form-dialog-title'>Update a Car</DialogTitle>
-                                        <DialogContent>
-                                            <DialogContentText>Car id: {car.id}</DialogContentText>
-                                            <CarForm id={car.id} />
-                                        </DialogContent>
-                                        <DialogActions>
-                                            <Button onClick={handleClose} color='primary'>Cancel</Button>
-                                        </DialogActions>
-                                    </Dialog>
-                                </Card>
-                            </Grid>
-                       ))}
-                    </Grid>
-                </Container>
-            </main>
-        </ThemeProvider>
-    );
+                </main>
+            </ThemeProvider>
+        );
+    } else {
+        return (
+            <div>
+                <h3>Please Sign In to View Your Drone Collection</h3>
+            </div>
+        )
+    };
 }
 
